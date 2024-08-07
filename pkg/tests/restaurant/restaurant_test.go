e1dd4079c73ead989450e82760b2cb611b7db3ec
Mukul Mantosh <mukulmantosh@yahoo.com>
2024-08-07 20:55:19 +0530

    Unit Tests

--- pkg/tests/restaurant/restaurant_test.go
+++ pkg/tests/restaurant/restaurant_test.go
@@ -5,6 +5,7 @@ import (
 	"Go_Food_Delivery/pkg/handler/restaurant"
 	"Go_Food_Delivery/pkg/tests"
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/go-faker/faker/v4"
 	"github.com/stretchr/testify/assert"
@@ -43,7 +44,7 @@ func generateData(restaurant FakeRestaurant) (*bytes.Buffer, string, error) {
 
 }
 
-func TestAddRestaurant(t *testing.T) {
+func TestRestaurant(t *testing.T) {
 	t.Setenv("STORAGE_TYPE", "local")
 	t.Setenv("STORAGE_DIRECTORY", "uploads")
 	t.Setenv("LOCAL_STORAGE_PATH", "./tmp")
@@ -51,6 +52,7 @@ func TestAddRestaurant(t *testing.T) {
 	testServer := handler.NewServer(testDB)
 	restaurant.NewRestaurant(testServer, "/restaurant")
 
+	var RestaurantResponseID int64
 	name := faker.Name()
 	file := []byte{10, 10, 10, 10, 10} // fake image bytes
 	description := faker.Paragraph()
@@ -72,16 +74,57 @@ func TestAddRestaurant(t *testing.T) {
 		t.Fatalf("Error generating form-data: %v", err)
 	}
 
-	t.Run("Restaurant::should return 201 created", func(t *testing.T) {
+	t.Run("Restaurant::Create", func(t *testing.T) {
 
 		req, _ := http.NewRequest(http.MethodPost, "/restaurant/", body)
-		fmt.Println(req.URL.Path)
 		req.Header.Set("Content-Type", contentType)
 		w := httptest.NewRecorder()
 		testServer.Gin().ServeHTTP(w, req)
 		assert.Equal(t, http.StatusCreated, w.Code)
 
-		tests.Teardown(testDB)
 	})
+
+	t.Run("Restaurant::Listing", func(t *testing.T) {
+
+		type RestaurantResponse struct {
+			RestaurantID int64  `json:"restaurant_id"`
+			Name         string `json:"name"`
+			StoreImage   string `json:"store_image"`
+			Description  string `json:"description"`
+			Address      string `json:"address"`
+			City         string `json:"city"`
+			State        string `json:"state"`
+			CreatedAt    string `json:"CreatedAt"`
+			UpdatedAt    string `json:"UpdatedAt"`
+		}
+
+		req, _ := http.NewRequest(http.MethodGet, "/restaurant/", nil)
+		req.Header.Set("Content-Type", "application/Json")
+		w := httptest.NewRecorder()
+		testServer.Gin().ServeHTTP(w, req)
+		assert.Equal(t, http.StatusOK, w.Code)
+
+		var restaurants []RestaurantResponse
+		err := json.Unmarshal(w.Body.Bytes(), &restaurants)
+		if err != nil {
+			t.Fatalf("Failed to decode response body: %v", err)
+		}
+
+		// set the restaurantID
+		RestaurantResponseID = restaurants[0].RestaurantID
+
+	})
+
+	t.Run("Restaurant::Delete", func(t *testing.T) {
+		url := fmt.Sprintf("/restaurant/%d", RestaurantResponseID)
+		req, _ := http.NewRequest(http.MethodDelete, url, nil)
+		req.Header.Set("Content-Type", "application/Json")
+		w := httptest.NewRecorder()
+		testServer.Gin().ServeHTTP(w, req)
+		assert.Equal(t, http.StatusNoContent, w.Code)
+	})
+
+	// Cleanup
+	tests.Teardown(testDB)
 
 }
