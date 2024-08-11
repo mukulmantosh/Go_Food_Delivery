efeee574d6e9de6fcfee394ea3fb1d2e7745a3b6
Mukul Mantosh <mukulmantosh@yahoo.com>
2024-08-11 08:02:31 +0530

    Add test for user login
    Implemented a new test case "User::Login" in user_test.go to validate the login functionality. This test verifies that a registered user can log in successfully and obtain a 200 OK response. The setup and teardown for the database are correctly handled to ensure isolated test runs.

--- pkg/tests/user/user_test.go
+++ pkg/tests/user/user_test.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	userModel "Go_Food_Delivery/pkg/database/models/user"
 	"Go_Food_Delivery/pkg/handler"
 	"Go_Food_Delivery/pkg/handler/user"
 	usr "Go_Food_Delivery/pkg/service/user"
@@ -34,6 +35,8 @@ func TestAddUser(t *testing.T) {
 		Password string `json:"password" faker:"password"`
 	}
 
+	var loggedInUser userModel.LoginUser
+
 	t.Run("User::Create", func(t *testing.T) {
 
 		var customUser FakeUser
@@ -43,13 +46,32 @@ func TestAddUser(t *testing.T) {
 			t.Fatal(err)
 		}
 
+		loggedInUser.Email = customUser.Email
+		loggedInUser.Password = customUser.Password
+
 		req, _ := http.NewRequest(http.MethodPost, "/user/", strings.NewReader(string(payload)))
 		req.Header.Set("Content-Type", "application/json")
 		w := httptest.NewRecorder()
 		testServer.Gin().ServeHTTP(w, req)
 		assert.Equal(t, http.StatusCreated, w.Code)
 
-		tests.Teardown(testDB)
 	})
+
+	t.Run("User::Login", func(t *testing.T) {
+
+		payload, err := json.Marshal(&loggedInUser)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req, _ := http.NewRequest(http.MethodPost, "/user/login", strings.NewReader(string(payload)))
+		req.Header.Set("Content-Type", "application/json")
+		w := httptest.NewRecorder()
+		testServer.Gin().ServeHTTP(w, req)
+		assert.Equal(t, http.StatusOK, w.Code)
+
+	})
+
+	tests.Teardown(testDB)
 
 }
