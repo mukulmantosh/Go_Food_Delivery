33f1851884b853442e0192a21519be87c0cc6498
Mukul Mantosh <mukul.mantosh@jetbrains.com>
2024-09-27 16:24:07 +0530

    Tests

--- pkg/tests/delivery/delivery_test.go
+++ pkg/tests/delivery/delivery_test.go
@@ -11,8 +11,10 @@ import (
 	"Go_Food_Delivery/pkg/tests"
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/pquerna/otp/totp"
 	"github.com/stretchr/testify/assert"
 	"github.com/testcontainers/testcontainers-go"
@@ -24,6 +26,29 @@ import (
 	"testing"
 	"time"
 )
+
+func parseToken(tokenString string) bool {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// return jwt secret key
+		return []byte("sample123"), nil
+	})
+	if err != nil {
+		return false
+	}
+
+	if _, ok := token.Claims.(jwt.MapClaims); ok {
+		return true
+	} else {
+		return false
+
+	}
+
+}
 
 func TestDeliveryUser(t *testing.T) {
 	t.Setenv("APP_ENV", "TEST")
@@ -97,6 +122,53 @@ func TestDeliveryUser(t *testing.T) {
 		}
 		assert.True(t, deliveryService.ValidateOTP(ctx, secret, otp), true)
 
+	})
+
+}
+
+func TestDeliveryGenerateJWT(t *testing.T) {
+	t.Setenv("APP_ENV", "TEST")
+	t.Setenv("STORAGE_TYPE", "local")
+	t.Setenv("STORAGE_DIRECTORY", "uploads")
+	t.Setenv("LOCAL_STORAGE_PATH", "./tmp")
+	t.Setenv("JWT_SECRET_KEY", "sample123")
+	testDB := tests.Setup()
+	AppEnv := os.Getenv("APP_ENV")
+	testServer := handler.NewServer(testDB, false)
+	middlewares := []gin.HandlerFunc{middleware.AuthMiddleware()}
+
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(60*time.Second))
+	defer cancel()
+
+	natsContainer, err := nats.Run(ctx, "nats:2.10.20", testcontainers.WithHostPortAccess(4222))
+
+	if err != nil {
+		t.Logf("failed to start NATS container: %s", err)
+		return
+	}
+	connectionString, err := natsContainer.ConnectionString(ctx)
+	if err != nil {
+		t.Log("NATS Connection String Error::", err)
+		return
+	}
+
+	// Connect NATS
+	natTestServer, _ := natsPkg.NewNATS(connectionString)
+
+	validate := validator.New()
+	userService := usr.NewUserService(testDB, AppEnv)
+	user.NewUserHandler(testServer, "/user", userService, validate)
+
+	deliveryService := delivery.NewDeliveryService(testDB, os.Getenv("APP_ENV"), natTestServer)
+	delv.NewDeliveryHandler(testServer, "/delivery", deliveryService, middlewares, validate)
+
+	t.Run("Delivery::User::GenerateJWT", func(t *testing.T) {
+		token, err := deliveryService.GenerateJWT(ctx, 1, "SAMPLE")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.True(t, parseToken(token), true)
 	})
 
 }
